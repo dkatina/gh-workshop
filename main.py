3fad1ade73a16c55742858619e1ef7840ef4a583
dkatina <108095842+dkatina@users.noreply.github.com>
2023-10-06 11:23:19 -0400

    developed a subtract function

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ def add(a, b):
 
 
 #we need to initialize a git repository (local)
+
+#~git branch to view all branches
+
+#~git branch <branch name> to create a new branch with that name
+
+#git checkout <branch name> to move to that branch
+
+def subtract(a, b):
+    return a - b
