cdbccd2b3277a72e463723c393fa88ae7c2466a4
dkatina <108095842+dkatina@users.noreply.github.com>
2023-10-06 11:44:23 -0400

    lifecycle comments

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ def subtract(a, b):
 #git merge <branch name> to bring all of those changes to the current branch
 
 #adding some more work that is not in my GH yet 
+
+
+#git init - (initialize local repo only needs to be done once)
+
+#git add . - Adds all your files to a staging process
+
+#git commit -m <message> - commits/save all files in the staging process with a message about the work
+
+#git push -u origin <branch name> url to GH-repo - Sends work to GH
